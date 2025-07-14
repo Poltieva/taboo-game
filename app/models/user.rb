87e094ce3fb91432a9f6f0160dc5425e1a9b2c57
Anna Poltieva <poltyeva.anna@gmail.com>
2025-07-14 14:24:56 +0300

    Add basic auth

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
+  has_secure_password
+  has_many :sessions, dependent: :destroy
+
+  normalizes :git, with: ->(e) { e.strip.downcase }
   has_many :player_games
   has_many :games, through: :player_games
 
